10e9a914d0a038721469cca13725b67a4609d0a7
Emre Eldemir <emreeldemir893@gmail.com>
2023-02-19 01:37:55 +0300

    RedBlackTree implementation added

---
+++ RedBlackTree/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        RedBlackTree redBlackTree = new RedBlackTree();
+
+        redBlackTree.insert(3);
+        redBlackTree.insert(8);
+        redBlackTree.insert(2);
+        redBlackTree.insert(5);
+        redBlackTree.insert(9);
+        redBlackTree.insert(7);
+
+        redBlackTree.print();
+
+    }
+
+}
