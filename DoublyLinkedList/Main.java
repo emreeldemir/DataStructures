b527bb32e45eafbb618ef813ab0c121494e9561b
Emre Eldemir <emreeldemir893@gmail.com>
2023-02-19 01:29:31 +0300

    DoublyLinkedList implementation added

---
+++ DoublyLinkedList/Main.java
@@ -0,0 +1,28 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        DoublyLinkedList<String> doublyLinkedList = new DoublyLinkedList<>();
+        doublyLinkedList.addFirst("Ali");
+        doublyLinkedList.addLast("Veli");
+        doublyLinkedList.addFirst("Mehmet");
+        doublyLinkedList.insert("Huseyin",2);
+        doublyLinkedList.printList();
+        System.out.println("************************");
+        doublyLinkedList.delete(3);
+        doublyLinkedList.printList();
+        System.out.println("************************");
+        System.out.println(doublyLinkedList.getFirstElement());
+        System.out.println("************************");
+        System.out.println(doublyLinkedList.getLastElement());
+        System.out.println("************************");
+        doublyLinkedList.update(2,"Oguzhan");
+        doublyLinkedList.printList();
+        System.out.println("************************");
+
+    }
+
+
+}
